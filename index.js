f5a943bb049441c981a81f010de5cfa775410328
nab5m <sk990420@naver.com>
2020-12-21 18:50:15 +0900

    부분 한글화

--- index.js
+++ index.js
@@ -81,6 +81,44 @@ const adminBro = new AdminBro({
       })],
     }
   ],
+  locale: {
+    language: 'ko',
+    translations: {
+      actions: {
+        new: '생성',
+        edit: '수정',
+        show: '상세',
+        list: '목록',
+      },
+      buttons: {
+        save: '저장',
+        filter: '필터',
+        filters: '필터',
+        applyChanges: '저장',
+        Reset: '리셋',
+        // We use i18next with its pluralization logic.
+        // confirmRemovalMany_1: 'Potwierdź usunięcie {{count}} rekordu',
+        // confirmRemovalMany_2: 'Potwierdź usunięcie {{count}} rekordów',
+        // ...
+      },
+      labels: {
+        Notice: '공지사항',
+        Profiles: '프로필',
+      },
+      properties: {
+        createdAt: '생성 시간',
+        updatedAt: '수정 시간',
+        order: '순서',
+        title: '제목',
+        contents: '내용',
+
+        name: '이름',
+        image: '사진',
+        age: '나이',
+        nickname: '닉네임'
+      },
+    }
+  },
 })
 
 const router = process.env.LOGIN_REQUIRED === 'true' ? AdminBroExpress.buildAuthenticatedRouter(adminBro, {
