c83af970992d48df78869d772214e4421a50c190
nab5m <sk990420@naver.com>
2020-12-21 10:15:23 +0900

    TSFI-34 TextEditor 컴포넌트: 정렬 툴바 추가

---
+++ TextEditor.jsx
@@ -0,0 +1,42 @@
+import { RichText } from '@admin-bro/design-system'
+
+export default function TextEditor(props) {
+  const { property, record, onChange } = props
+  const value = record.params[property.name]
+
+  const QuillToolbarOptions = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+
+    [{ align: ['', 'right', 'center'] }],
+    ['link', 'image', 'video'],
+
+    ['clean'], // remove formatting button
+  ]
+
+  return (
+    <RichText
+      quill={{
+        theme: 'snow',
+        modules: {
+          toolbar: QuillToolbarOptions,
+        }
+      }}
+      value={value}
+      onChange={(text) => {
+        console.log(text);
+        // here
+        onChange({
+          ...record,
+          params: {
+            ...record.params,
+            [property.name]: text,
+          }
+        })
+      }}
+    />
+  )
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env-local') })
+
 const AdminBro = require('admin-bro')
-const AdminBroFirebase = require('admin-bro-firebase')
+const AdminBroFirebase = require('@tirrilee/admin-bro-firebase')
 const AdminBroExpress = require('@admin-bro/express')
 const firebase = require("firebase/app");
 
@@ -8,14 +11,14 @@ const app = express()
 
 // 파이어베이스 웹 플랫폼 추가하면 나옴
 const firebaseConfig = {
-  apiKey: "your api key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://myside-7a4b6.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -28,9 +31,18 @@ const adminBro = new AdminBro({
     {
       collection: firebase.firestore().collection('Profiles'),
       schema: {
-        name: 'string',
+        name: 'richtext',
       },
-    },
+      options: {
+        properties: {
+          name: {
+            components: {
+              edit: AdminBro.bundle('./CustomEditor'),
+            },
+          },
+        },
+      },
+    }
   ],
 })
 
