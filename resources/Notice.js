d4ae22ce0371894c551218f0bf7ae59d3fea237a
nab5m <sk990420@naver.com>
2020-12-22 15:24:42 +0900

    설정 파일 분리

---
+++ resources/Notice.js
@@ -0,0 +1,28 @@
+const AdminBro = require('admin-bro')
+const {FireStore} = require('../FirebaseConfig')
+
+module.exports = {
+  collection: FireStore.collection('Notice'),
+  schema: {
+    order: 'number',
+    title: 'string',
+    contents: 'richtext',
+    createdAt: 'datetime',
+    updatedAt: 'datetime',
+  },
+  options: {
+    properties: {
+      contents: {
+        components: {
+          edit: AdminBro.bundle('../components/TextEditor'),
+        },
+      },
+      createdAt: {
+        isVisible: { list: true, show: true, edit: false },
+      },
+      updatedAt: {
+        isVisible: { list: true, show: true, edit: false },
+      },
+    },
+  },
+}
